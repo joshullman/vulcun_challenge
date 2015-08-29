649f003528584ac69154e30607a9d9d4dde5382a
Josh Ullman <joshullman@gmail.com>
2015-08-28 18:20:59 -0700

    add 10,000,000 user json file + modules.  Working on importing it to mongodb next

--- test_chat/index.js
+++ test_chat/index.js
@@ -1,14 +1,30 @@
-// var app = require('express')();
+var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 // NEED TO REPLACE THIS...HOW??
-// app.get('/', function(req, res){
-//   res.sendFile(__dirname + '/index.html');
-// });
+// CONTROLLER FOR EXPRESS THAT LOOKS LIKE SINATRA
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/login.html');
+});
 
+app.post('/', function(req, res){
+  res.sendFile(__dirname + '/register.html');
+});
+
+app.get('/chat', function(req, res){
+  res.sendFile(__dirname + '/chat.html');
+});
+
+
+
+// SOCKET IO
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
+  	console.log(msg)
+  	//
+  	// in here is where we send off the message to something somewhere
+  	//
     io.emit('chat message', msg);
   });
 });
---
+++ test_chat/push_users.js
@@ -0,0 +1,46 @@
+var Faker = require('Faker');
+var fs = require('fs');
+
+// Did this 5 times while changing the 'i' because of my computer's limitations
+
+fs.open('users.json', 'a', function(error, fd) {
+	for (i=8000001;i<=9000000;i++) {
+		var first_name = Faker.name.firstName();
+		var last_name = Faker.name.lastName();
+		var user = {
+			'id': i,
+			'full_name': first_name + " " + last_name,
+			'email': first_name + last_name + "@email.com",
+			'password': 'password',
+			'city': Faker.address.city()
+		}
+		fs.write(fd, JSON.stringify(user) + "\n", function (err) {
+		  if (err) throw err;
+		});
+		console.log(i);
+	}
+	fs.close(fd, function() {
+		console.log('file closed');
+	});
+});
+
+fs.open('users.json', 'a', function(error, fd) {
+	for (i=9000001;i<=10000000;i++) {
+		var first_name = Faker.name.firstName();
+		var last_name = Faker.name.lastName();
+		var user = {
+			'id': i,
+			'full_name': first_name + " " + last_name,
+			'email': first_name + last_name + "@email.com",
+			'password': 'password',
+			'city': Faker.address.city()
+		}
+		fs.write(fd, JSON.stringify(user) + "\n", function (err) {
+		  if (err) throw err;
+		});
+		console.log(i);
+	}
+	fs.close(fd, function() {
+		console.log('file closed');
+	});
+});
