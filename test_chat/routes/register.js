be313c467869bf3dd8df88509ddfff908da6e72d
Josh Ullman <joshullman@gmail.com>
2015-08-30 21:08:48 -0700

    Deleted database files and closed Mongo zombie server.

--- test_chat/routes/register.js
+++ test_chat/routes/register.js
@@ -5,4 +5,8 @@ router.get('/register', function(req, res){
   res.render('register', { title: 'Express' });
 });
 
+router.post('/register', function(req, res){
+	req.query
+})
+
 module.exports = router;
